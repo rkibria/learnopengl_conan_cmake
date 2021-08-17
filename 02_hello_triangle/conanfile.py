25b843aaab02a717b4f4359731ef2ef9ee492329
Raihan Kibria <rkibria@users.noreply.github.com>
2021-08-17 19:15:40 +0100

    Example 02

---
+++ 02_hello_triangle/conanfile.py
@@ -0,0 +1,14 @@
+from conans import ConanFile, tools, CMake
+
+class LearnOpenGLHelloTriangleConan(ConanFile):
+    name = "LearnOpenGLHelloWindow"
+    version = "0.1"
+    requires = "glfw/3.3.4", "glad/0.1.34"
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "cmake_find_package"
+    url = "https://learnopengl.com/Getting-started/Hello-Window"
+
+    def build(self):
+        cmake = CMake(self, generator="Ninja")
+        cmake.configure()
+        cmake.build()
